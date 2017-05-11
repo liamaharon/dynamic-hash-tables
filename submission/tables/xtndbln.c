adf9c8cc68deb18297cbcd4d1a6c5b8cd4712cc7
Liam <liam.aharon@hotmail.com>
2017-05-11 18:06:36 +1000

    Implemented split_bucket()

--- submission/tables/xtndbln.c
+++ submission/tables/xtndbln.c
@@ -92,64 +92,67 @@ struct xtndbln_table {
 // need to set this up eventually, changing lsat bit where keys is reinserted
 // to reinsert every key from that bucket
  // split the bucket in 'table' at address 'address', growing table if necessary
- static void split_bucket(XtndblNHashTable *table, int address) {
- //
- // 	// FIRST,
- // 	// do we need to grow the table?
- // 	if (table->buckets[address]->depth == table->depth) {
- // 		// yep, this bucket is down to its last pointer
- // 		double_table(table);
- // 	}
- // 	// either way, now it's time to split this bucket
- //
- //
- // 	// SECOND,
- // 	// create a new bucket and update both buckets' depth
- // 	Bucket *bucket = table->buckets[address];
- // 	int depth = bucket->depth;
- // 	int first_address = bucket->id;
- //
- // 	int new_depth = depth + 1;
- // 	bucket->depth = new_depth;
- //
- // 	// new bucket's first address will be a 1 bit plus the old first address
- // 	int new_first_address = 1 << depth | first_address;
- // 	Bucket *newbucket = new_bucket(new_first_address, new_depth);
- // 	table->stats.nbuckets++;
- //
- // 	// THIRD,
- // 	// redirect every second address pointing to this bucket to the new bucket
- // 	// construct addresses by joining a bit 'prefix' and a bit 'suffix'
- // 	// (defined below)
- //
- // 	// suffix: a 1 bit followed by the previous bucket bit address
- // 	int bit_address = rightmostnbits(depth, first_address);
- // 	int suffix = (1 << depth) | bit_address;
- //
- // 	// prefix: all bitstrings of length equal to the difference between the new
- // 	// bucket depth and the table depth
- // 	// use a for loop to enumerate all possible prefixes less than maxprefix:
- // 	int maxprefix = 1 << (table->depth - new_depth);
- //
- // 	int prefix;
- // 	for (prefix = 0; prefix < maxprefix; prefix++) {
- //
- // 		// construct address by joining this prefix and the suffix
- // 		int a = (prefix << new_depth) | suffix;
- //
- // 		// redirect this table entry to point at the new bucket
- // 		table->buckets[a] = newbucket;
- // 	}
- //
- // 	// FINALLY,
- // 	// filter the key from the old bucket into its rightful place in the new
- // 	// table (which may be the old bucket, or may be the new bucket)
- //
- // 	// remove and reinsert the key
- // 	int64 key = bucket->key;
- // 	bucket->full = false;
- // 	reinsert_key(table, key);
- }
+static void split_bucket(XtndblNHashTable *table, int address) {
+	assert(table);
+
+	// FIRST,
+	// do we need to grow the table?
+	if (table->buckets[address]->depth == table->depth) {
+		// yep, this bucket is down to its last pointer
+		double_table(table);
+	}
+	// either way, now it's time to split this bucket
+
+	// SECOND,
+	// create a new bucket and update both buckets' depth
+	Bucket *bucket = table->buckets[address];
+	int depth = bucket->depth;
+	int first_address = bucket->id;
+	int bucketsize = table->bucketsize;
+
+	int new_depth = depth + 1;
+	bucket->depth = new_depth;
+
+	// new bucket's first address will be a 1 bit plus the old first address
+	int new_first_address = 1 << depth | first_address;
+	Bucket *newbucket = new_bucket(new_first_address, new_depth, bucketsize);
+
+	// THIRD,
+	// redirect every second address pointing to this bucket to the new bucket
+	// construct addresses by joining a bit 'prefix' and a bit 'suffix'
+	// (defined below)
+
+	// suffix: a 1 bit followed by the previous bucket bit address
+	int bit_address = rightmostnbits(depth, first_address);
+	int suffix = (1 << depth) | bit_address;
+
+	// prefix: all bitstrings of length equal to the difference between the new
+	// bucket depth and the table depth
+	// use a for loop to enumerate all possible prefixes less than maxprefix:
+	int maxprefix = 1 << (table->depth - new_depth);
+
+	int prefix;
+	for (prefix = 0; prefix < maxprefix; prefix++) {
+
+		// construct address by joining this prefix and the suffix
+		int a = (prefix << new_depth) | suffix;
+
+		// redirect this table entry to point at the new bucket
+		table->buckets[a] = newbucket;
+	}
+
+	// FINALLY,
+	// filter the keys from the old bucket into their rightful place in the new
+	// table (which may be the old bucket, or may be the new bucket)
+
+	// remove and reinsert the keys
+	int index;
+	while (bucket->nkeys > 0) {
+		index = bucket->nkeys -= 1;
+		int64 key = bucket->keys[index];
+		reinsert_key(table, key);
+	}
+}
 
  /* * * *
   * all functions
